0d177039a4a7a8ed878118335ceeee0f104e3b98
Emily Beecher <ebeecher@uoregon.edu>
2019-06-11 16:04:12 -0700

    finalized analytical dataset???

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -11,38 +11,52 @@
 
 library(readr)
 
-sexharass <- read_fwf("analysis/input/sexharrass_fwf_fixed.txt",
-                      col_positions = fwf_positions(start=c( 1,92,94,96,98,100,102,104,106,108,110,112,123,125,127,129,131),
-                                                    end=  c(21,93,95,97,99,101,103,105,107,109,111,121,124,126,128,130,132),
-                                                    col_names=c("controlblock","Q27A","Q27B","Q27C","Q27D","Q27E","Q27F","Q27G","Q27H","Q27I","Q27J","Q28","Q30A","Q30B","Q30C","Q30D","Q30E"),
-                                                    trims_ws=FALSE))
+sexharass <- read_fwf("analysis/input/sexharrass_fwf_fixed.txt", trim_ws = FALSE,
+                      col_positions = fwf_positions(start=c( 1,66,67,68,69,70,71,72,73,92,94,96,98,100,
+                                                             102,104,106,108,110,112,123,125,127,129,131),
+                                                    end=  c(21,66,67,68,69,70,71,72,73,93,95,97,99,101,
+                                                            103,105,107,109,111,121,124,126,128,130,132),
+                                                    col_names=c("controlblock","Q20A","Q20B","Q20C","Q20D",
+                                                                "Q20E","Q20F","Q20G","Q20H","Q27A","Q27B",
+                                                                "Q27C","Q27D","Q27E","Q27F","Q27G","Q27H",
+                                                                "Q27I","Q27J","Q28","Q30A","Q30B","Q30C",
+                                                                "Q30D","Q30E")))
 
+#Q20: did they experience sex harassment with degrees of frequency 
+#(282 people experienced an incident and 7799 didn't)
+sexharass$incident <- NA
+sexharass$incident[sexharass$incident==" "] <- NA
+sexharass$incident <- sexharass$Q20A!="1" & sexharass$Q20B!="1" & sexharass$Q20C!="1" & 
+  sexharass$Q20D!="1" & sexharass$Q20E!="1" & sexharass$Q20F!="1" & 
+  sexharass$Q20G!="1" & sexharass$Q20H!="1"
+
+#Q28: positive, negative, neutral outcome, asked of everyone
+sexharass$work_worse <- substr(sexharass$Q28, 1, 1)=="1"
+sexharass$bad_ref <- substr(sexharass$Q28, 2, 2)=="1"
+sexharass$transfer <- substr(sexharass$Q28, 3, 3)=="1"
+sexharass$fired <- substr(sexharass$Q28, 4, 4)=="1"
+sexharass$quit_newjob <- substr(sexharass$Q28, 5, 5)=="1"
+sexharass$quit_nojob <- substr(sexharass$Q28, 6, 6)=="1"
+sexharass$work_better <- substr(sexharass$Q28, 7, 7)=="2"
+sexharass$work_nochange <- substr(sexharass$Q28, 8, 8)=="3"
+
+#Q30: kind of investigation pursued, asked of people who had an incident
 sexharass <- na.omit(sexharass)
-sexharass$invest <- NA
-sexharass$invest[sexharass$Q30A=="1" | sexharass$Q30A=="11"] <- "internalbetter"
-sexharass$invest[sexharass$Q30A=="2" | sexharass$Q30A=="12"] <- "internalneutral"
-sexharass$invest[sexharass$Q30A=="3" | sexharass$Q30A=="13"] <- "internalworse"
 
-sexharass$invest[sexharass$Q30B=="1" | sexharass$Q30B=="11"] <- "outsidebetter"
-sexharass$invest[sexharass$Q30B=="2" | sexharass$Q30B=="12"] <- "outsideneutral"
-sexharass$invest[sexharass$Q30B=="3" | sexharass$Q30B=="13"] <- "outsideworse"
+sexharass$internalinvest <- NA
+sexharass$internalinvest <- sexharass$Q30A!="  "
 
-sexharass$invest[sexharass$Q30C=="1" | sexharass$Q30C=="11"] <- "unionbetter"
-sexharass$invest[sexharass$Q30C=="2" | sexharass$Q30C=="12"] <- "unionneutral"
-sexharass$invest[sexharass$Q30C=="3" | sexharass$Q30C=="13"] <- "unionworse"
+sexharass$outsideinvest <- NA
+sexharass$outsideinvest <- sexharass$Q30B!="   "
 
-sexharass$invest[sexharass$Q30D=="1" | sexharass$Q30D=="11"] <- "EEObetter"
-sexharass$invest[sexharass$Q30D=="2" | sexharass$Q30D=="12"] <- "EEOneutral"
-sexharass$invest[sexharass$Q30D=="3" | sexharass$Q30D=="13"] <- "EEOworse"
+sexharass$unioninvest <- NA
+sexharass$unioninvest <- sexharass$Q30C!="   "
 
-sexharass$invest[sexharass$Q30E=="1" | sexharass$Q30E=="11"] <- "otherbetter"
-sexharass$invest[sexharass$Q30E=="2" | sexharass$Q30E=="12"] <- "otherneutral"
-sexharass$invest[sexharass$Q30E=="3" | sexharass$Q30E=="13"] <- "otherworse"
+sexharass$eeoinvest <- NA
+sexharass$eeoinvest <- sexharass$Q30D!="   "
 
-sexharass$invest <- factor(sexharass$invest, levels=c("internalworse", "internalneutral", "internalbetter","outsideworse", "outsideneutral", "outsidebetter","unionworse", "unionneutral", "unionbetter","EEOworse", "EEOneutral", "EEObetter","otherworse", "otherneutral", "otherbetter"), ordered=TRUE)
+sexharass$otherinvest <- NA
+sexharass$otherinvest <- sexharass$Q30E!="   "
 
 
-#sexharass$action <- NA
-#sexharass$action[sexharass$Q27A==]
-#code question 27 as actions taken, better worse neutral
-#code question 28 somehow??? collapse down to positive negative neutral outcomes
+#how do I drop the cases for the outcome variables for people who didn't experience an incident?
